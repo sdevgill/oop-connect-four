1de1bbc0a410ac22c568d030516393e7f03334ed
Sukhdev Gill <26435947+sukhdevgill@users.noreply.github.com>
2022-06-20 12:46:55 +0100

    Pass tests in test/cursor-spec.js with cursor movements

--- class/cursor.js
+++ class/cursor.js
@@ -22,12 +22,41 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
-  left() {
-    // Move cursor left
+  // Create method to reset the Screen after moving the cursor
+  resetScreen = (r, c) => {
+    this.resetBackgroundColor();
+    this.row += r;
+    this.col += c;
+    this.setBackgroundColor();
+    Screen.render();
   }
 
-  right() {
+  up = () => {
+    // Move cursor up
+    if (this.row > 0 && this.row < this.numRows) {
+      this.resetScreen(-1, 0);
+    }
+  }
+
+  down = () => {
+    // Move cursor down
+    if (this.row >= 0 && this.row < (this.numRows - 1)) {
+      this.resetScreen(1, 0);
+    }
+  }
+
+  left = () => {
+    // Move cursor left
+    if (this.col > 0 && this.col < this.numCols) {
+      this.resetScreen(0, -1);
+    }
+  }
+
+  right = () => {
     // Move cursor right
+    if (this.col >= 0 && this.col < (this.numCols - 1)) {
+      this.resetScreen(0, 1);
+    }
   }
 
 }
